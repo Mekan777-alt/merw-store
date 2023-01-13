8792d822d0347c44e6ef5ef8d638b3eade9eef5c
Mekan777-alt <mekan2678@gmail.com>
2023-01-13 14:00:38 +0300

    add models to products app

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория продуктов'
+        verbose_name_plural = 'Категория продуктов'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    quantity = models.PositiveIntegerField(default=0)
+    description = models.CharField(max_length=400)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    image = models.ImageField(upload_to='products_image', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Продукты'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return f'{self.category} - {self.name}'
